70b813a51978959276cbac1afcbc09197e53c6b0
Magdalena Gontarz <magda1428@gmail.com>
2016-03-20 16:42:29 +0100

    new functionality in utils and feedback modified

--- feedback.py
+++ feedback.py
@@ -7,6 +7,7 @@ from obci.analysis.balance.wii_analysis import *
 
 from obci.exps.ventures.analysis import analysis_baseline
 from obci.exps.ventures.analysis import analysis_helper
+from utils.wiiboard_utils import maximal_sway
  
 #from obci.acquisition import acquisition_helper
 import numpy as np
@@ -14,6 +15,9 @@ import matplotlib.patches as patches
 import pprint
 import pandas as pd
 import ast
+import matplotlib
+
+
 
 PERSON = 'dk'
 BASELINE_FILE_PATH = 'dane/wii_' + PERSON + '/' #full path
@@ -26,7 +30,8 @@ else:
     BASELINE_FILE_NAME = 'new_tech_baseline_with_feedback_2016-03-02_15-02-45' #file name without extension
     FILE_NAME = 'new_tech_sway_stay_with_feedback_2016-03-02_15-05-01' #file name without extension
     
-
+matplotlib.rc('xtick', labelsize=15) 
+matplotlib.rc('ytick', labelsize=15)
 py.style.use('ggplot')
 
 class WiiSway(object):
@@ -81,26 +86,28 @@ class WiiSway(object):
                    )
                    for idx, i in enumerate(smart_tags) if int(i.get_end_tag()['desc']['type'])])
         df.columns = ['idx', 'is_done','direction','level']
-        directions = (df.tail(4)['direction'].tolist(), df.tail(4)['idx'].tolist())
-        print df
-        print directions
-        self.x = [self.XSCALE*i.get_channel_samples('x') for ind,i in enumerate(smart_tags)]
-        self.y = [self.YSCALE*i.get_channel_samples('y') for ind,i in enumerate(smart_tags)]
-        return zip(self.x, self.y), directions
+        info = (df.tail(4)['direction'].tolist(), df.tail(4)['idx'].tolist(), df.tail(4)['level'].tolist())
         
-        
-    
 
-    def max_sway(self):
+        x = [self.XSCALE*i.get_channel_samples('x') for ind,i in enumerate(smart_tags)]
+        y = [self.YSCALE*i.get_channel_samples('y') for ind,i in enumerate(smart_tags)]
+        return zip(x, y), info
+
+    def max_sway(self, x, y, way=1):
         "maximal sway from given *direction* averaged over trials"
-        max_sway, max_AP, max_ML = wii_max_sway_AP_MP(self.x, self.y)
-        return max_sway, max_AP, max_ML
+        if way:
+            max_sway, max_AP, max_ML = wii_max_sway_AP_MP(x, y)
+            return max_sway
+        else:
+            max_AP, max_ML = maximal_sway(x, y)
+            return np.sqrt(max_AP**2+max_ML**2)
+        
         
     def plot_movement(self, x, y, name, show=False):
         xa, _, _, yb, xc, yc = self.get_baseline_points()
         py.figure()
         time = np.linspace(0, len(x)/self.fs, len(x))
-        py.suptitle(name)
+        py.suptitle(name, fontsize=30)
         ax1 = py.subplot(221)
         ax1.set_ylim((-self.lim_Y,self.lim_Y))
         ax2 = py.subplot(223)
@@ -109,16 +116,16 @@ class WiiSway(object):
         ax3.set_xlim((-self.lim_X,self.lim_X))
         ax3.set_ylim((-self.lim_Y,self.lim_Y))
         ax1.plot(time, x)
-        ax1.set_ylabel('position COPx [cm]')
-        ax1.set_title('COPx position')
+        ax1.set_ylabel('position COPx [cm]', fontsize=30)
+        ax1.set_title('COPx position', fontsize=30)
         ax2.plot(time, y)
-        ax2.set_ylabel('position COPy [cm]')
-        ax2.set_xlabel('Time [s]')
-        ax2.set_title('COPy position')
+        ax2.set_ylabel('position COPy [cm]', fontsize=30)
+        ax2.set_xlabel('Time [s]', fontsize=30)
+        ax2.set_title('COPy position', fontsize=30)
         ax3.plot(x,y)
-        ax3.set_ylabel('position COPy [cm]')
-        ax3.set_xlabel('position COPx [cm]')
-        ax3.set_title('COP position')
+        ax3.set_ylabel('position COPy [cm]', fontsize=30)
+        ax3.set_xlabel('position COPx [cm]', fontsize=30)
+        ax3.set_title('COP position', fontsize=30)
         ax3.add_patch(patches.Rectangle(
                      (self.XSCALE * xc, self.YSCALE * yc),   # (x,y)
                       2*xa*self.XSCALE,          # width
@@ -136,11 +143,11 @@ class OnePlot(WiiSway):
         fig=py.figure()
         ax = fig.add_subplot(111, aspect='equal')
         for i  in range(len(properties[0])):
-            x = signals[directions[1][i]][0]
-            y = signals[directions[1][i]][1]
+            x = signals[properties[1][i]][0]
+            y = signals[properties[1][i]][1]
             ax.plot(x, y, 'r')
-            ax.set_ylabel('position COPy [cm]')
-            ax.set_xlabel('position COPx [cm]')
+            ax.set_ylabel('position COPy [cm]', fontsize=20)
+            ax.set_xlabel('position COPx [cm]', fontsize=20)
             ax.set_xlim((-self.lim_X, self.lim_X))
             ax.set_ylim((-self.lim_Y, self.lim_Y))
         ax.add_patch(patches.Rectangle(
@@ -148,7 +155,7 @@ class OnePlot(WiiSway):
                       2*xa*self.XSCALE,          # width
                       2*yb*self.YSCALE,          # height
                       ))
-        ax.set_title(PERSON)
+        ax.set_title(PERSON, fontsize=25)
         py.tight_layout()
         py.show()
         #py.savefig('images/'+'feedback_allDir_%s_%s' % (name,PERSON))
@@ -156,20 +163,26 @@ class OnePlot(WiiSway):
 
 if __name__ == '__main__':
     wiisway = WiiSway(FILE_PATH)
-    signals, directions= wiisway.extract_signal_fragments()
+    signals, properties= wiisway.extract_signal_fragments()
     pp = pprint.PrettyPrinter(indent=4) 
-    pp.pprint(directions)
+    pp.pprint(properties)
+   
     
-    many = 0
+    many = 1
     if many:
-        for i in range(len(directions[0])):
-            x = signals[directions[1][i]][0]
-            y = signals[directions[1][i]][1]
-            wiisway.plot_movement(x,y, directions[0][i], show=False)
+        for i in range(len(properties[0])):
+            x = signals[properties[1][i]][0]
+            y = signals[properties[1][i]][1]
+            w1 = wiisway.max_sway(x,y)
+            w2 = wiisway.max_sway(x,y, way=0)
+            #print '{}:\t{}\t{}'.format(properties[0][i], w1, w2)
+            print '{}:\t{}'.format(properties[0][i], properties[2][i])
+            #wiisway.plot_movement(x,y, properties[0][i], show=False)
     else:
         wiisway = OnePlot(FILE_PATH)
-        signals, directions= wiisway.extract_signal_fragments()
-        wiisway.plot_movement(signals, directions)
+        signals, properties= wiisway.extract_signal_fragments()
+        wiisway.plot_movement(signals, properties)
+        
 
         
     
